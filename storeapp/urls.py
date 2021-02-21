11bf2a3367c68277d19867afacc6e4a65b43e17e
George Floarea <floareageorge@gmail.com>
2021-02-21 11:33:54 +0200

    Added views for viewing categories and products

--- storeapp/urls.py
+++ storeapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import categories_view
+from .views import categories_view, category_view
 
 app_name = 'storeapp'
 
 urlpatterns = [
-    path('categories/', categories_view)
+    path('categories/', categories_view, name='categories'),
+    path('categories/<int:category_id>/', category_view, name='view_category')
 ]
--- storeapp/views.py
+++ storeapp/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from .models import Category, Product
 
-# Create your views here.
 
 def categories_view(request):
-    return render(request, 'storeapp/categories.html')
+    categories = Category.objects.filter().all()
+    return render(request, 'storeapp/categories.html', {
+        "categories": categories
+    })
+
+
+def category_view(request, category_id):
+    if not Category.objects.filter(pk=category_id).exists():
+        raise Http404("Category does not exist!")
+
+    products = Product.objects.filter(category__pk=category_id).all()
+    return render(request, 'storeapp/products.html', {
+        "products": products
+    })
