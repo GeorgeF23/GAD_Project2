6a4045cfb188f7686369287e3703e6e5857d90b4
George Floarea <floareageorge@gmail.com>
2021-02-21 16:39:43 +0200

    Modified product.html. The details about the product are now in a bootstrap card. Added 'description' field to Product model

--- storeapp/models.py
+++ storeapp/models.py
@@ -22,6 +22,7 @@ class Product(models.Model):
 
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
