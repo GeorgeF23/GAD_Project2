736321257803149d2fc5c0819c1a679a9f976b44
George Floarea <floareageorge@gmail.com>
2021-02-26 13:44:35 +0200

    Created Order and OrderItem models.
    OrderItem is a intermediary table for the many to many relationship between Order and Product

--- storeapp/admin.py
+++ storeapp/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Order, OrderItem
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'category')
 
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('client', 'address')
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'final_price')
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
--- storeapp/models.py
+++ storeapp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.contrib.auth import get_user_model
+
+AuthUserModel = get_user_model()
 
 
 class Category(models.Model):
@@ -29,3 +32,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    products = models.ManyToManyField(Product, through='OrderItem')
+    client = models.ForeignKey(AuthUserModel, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    final_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    created_at = models.DateTimeField(auto_now_add=True)
